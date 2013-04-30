72aa85cfc855cb186a332389898898a0ee28019a
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-04-30 21:04:39 +0200

    _

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/algo.generic "0.1.1"]
                  [potemkin "0.2.2"]]
-  
+
   :profiles {:dev
              {:dependencies [[org.clojure/tools.trace "0.7.5"]
                              [midje "1.5.1"]]}})
